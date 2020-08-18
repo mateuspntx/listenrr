2fcaf3d61f3207193a901c19d1596cc31d896199
mateuspl <>
2020-08-18 19:04:28 -0300

    feat: added switch theme function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import GlobalStyle from '../styles/global';
+import darkTheme from '../styles/themes/dark';
+import lightTheme from '../styles/themes/light';
+
+const Listenrr = ({ Component, pageProps }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const darkMode = useDarkMode(true);
+  const theme = darkMode.value ? darkTheme : lightTheme;
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <button onClick={darkMode.enable}>DARK MODE</button>
+      <button onClick={darkMode.disable}>LIGHT MODE</button>
+      {isMounted && <Component {...pageProps} />}
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default Listenrr;
---
+++ styles/themes/light.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'light',
+
+  colors: {
+    background: '#F8F8F8',
+    logoText: '#464646',
+    primary: '#333333',
+    secondary: '#848484',
+  }
+};
