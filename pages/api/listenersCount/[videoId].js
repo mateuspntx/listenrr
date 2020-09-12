3955d10e2c2fd05f0de345e4f9d43ea2abddfc8f
mateuspntx <mateuspntx@gmail.com>
2020-09-12 02:12:08 -0300

    feat: fetch livestream concurrent viewers data

---
+++ pages/api/listenersCount/[videoId].js
@@ -0,0 +1,23 @@
+export default async (req, res) => {
+
+  const key = process.env.YOUTUBE_API_KEY;
+
+  const {
+    query: { videoId},
+  } = req;
+
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${videoId}&fields=items%2FliveStreamingDetails%2FconcurrentViewers&key=${key}`
+
+  const data = await fetch(url);
+  const { items } = await data.json();
+
+  try {
+    return res.json(items);
+  } 
+  
+  catch {
+    return res.json(
+      { message: 'Can\'t get listeners count at this moment'
+    });
+  }
+}
