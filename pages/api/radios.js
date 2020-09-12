9d85855dfbe3bb5ff2f21dd85413d0fdc3bbc246
mateuspntx <mateuspntx@gmail.com>
2020-09-12 01:38:44 -0300

    feat: fetching Youtube livestreams data

---
+++ pages/api/radios.js
@@ -0,0 +1,30 @@
+export default async (req, res) => {
+  
+  //Filters API can get = relevance (default), viewCount, videoCount, rating, date, title
+  //Max results API can get = 50
+
+  const key = process.env.YOUTUBE_API_KEY;
+
+  const {
+    query: { 
+      order,
+      maxResults,
+     },
+  } = req;
+
+  const defaultParams = `part=snippet&videoCategoryId=10&eventType=live`
+  const url = `https://www.googleapis.com/youtube/v3/search?${defaultParams}&maxResults=${maxResults}&order=${order}&type=video&key=${key}`
+
+  const data = await fetch(url);
+  const { items } = await data.json();
+
+  try {
+    return res.json(items);
+  } 
+  
+  catch {
+    return res.json(
+      { message: 'Can\'t get radios list at this moment'
+    });
+  }
+}
