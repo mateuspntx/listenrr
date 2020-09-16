c716e8c8f7ccdd559dacd693f083d030016e8a3d
mateuspntx <mateuspntx@gmail.com>
2020-09-16 18:28:49 -0300

    chore: created ShimmerEffect animation

---
+++ components/Skeletons/ShimmerEffect/index.js
@@ -0,0 +1,21 @@
+import { css } from 'styled-components';
+
+export const ShimmerEffect = css`
+  background-image: linear-gradient(
+    -90deg,
+    ${props => props.theme.colors.shimmerEffect} 0%,
+    #00000020 50%,
+    ${props => props.theme.colors.shimmerEffect} 100%
+  );
+  
+  background-size: 400% 400%;
+  animation: shimmer 1.2s ease-in-out infinite;
+  @keyframes shimmer {
+    0% {
+      background-position: 0% 0%;
+    }
+    100% {
+      background-position: -135% 0%;
+    }
+  }
+`;
