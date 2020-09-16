ce9948b6ffdfa13dd751c2472d247059ecd1c74d
mateuspntx <mateuspntx@gmail.com>
2020-09-16 01:21:06 -0300

    feat: updates cached data day in day out

--- services/api/index.js
+++ services/api/index.js
@@ -4,28 +4,33 @@ const api = axios.create({
   baseURL: process.env.API_BASE_URL || 'http://localhost:3000',
 });
 
-const LocalStorageName = 'LSTNR_cachedRadiosList';
+const LOCAL_STORAGE_NAME = 'LSTNR_cachedRadiosList';
+const LOCAL_STORAGE_DATE = 'LSTNR_Date';
 
 async function getRadios (filter, maxResults) {
+
+  const date = new Date();
+  const todayDate = date.getDate();
+
   try {
-    if(!localStorage.getItem(LocalStorageName)) {
+    if(localStorage.getItem(LOCAL_STORAGE_DATE) != todayDate) {
       await api.get(`/api/radios?order=${filter}&maxResults=${maxResults}`)
         .then(res => {
           const { items } = res.data;
           
-          localStorage.setItem(LocalStorageName, JSON.stringify(items));
+          localStorage.setItem(LOCAL_STORAGE_DATE, todayDate);
+          localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(items));
           
           return items;
 
         })
     }
-    
-    return JSON.parse(localStorage.getItem(LocalStorageName));
-    
+
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME));
   }
 
   catch (e) {
-    return e;
+    throw e;
   }
 }
 
