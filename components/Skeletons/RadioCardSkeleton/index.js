82280e0607f1f680c143ffd2d8dd8e12a0cd925e
mateuspntx <mateuspntx@gmail.com>
2020-09-16 18:00:21 -0300

    feat: created RadioCard Skeleton for Shimmer Effect

---
+++ components/Skeletons/RadioCardSkeleton/index.js
@@ -0,0 +1,13 @@
+import { RadioCardDiv, Cover, Title, Label } from './styles';
+
+const RadioCardSkeleton = () => {
+  return (
+    <RadioCardDiv>
+      <Cover />
+      <Title />
+      <Label />
+    </RadioCardDiv>
+  )
+}
+
+export default RadioCardSkeleton;
---
+++ components/Skeletons/RadioCardSkeleton/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Cover = styled.div`
+    background: ${props => props.theme.colors.shimmerEffect };
+    background-size: cover;
+    background-position: center;
+    width: 310px;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+
+    img {
+        opacity: 0;
+        width: 3rem;
+        filter: drop-shadow(0 0 5px rgba(0,0,0,.555));
+        transition: .2s;
+    }
+`
+
+export const Title = styled.h1`
+    margin-left: 0;
+    width: 80%;
+    height: 25px;
+    font-size: 1.7rem;
+    margin-top: 1.2rem;
+    font-weight: 500;
+    transition: color .5s;
+    background: ${props => props.theme.colors.shimmerEffect};
+    border-radius: 5px;
+`
+
+export const Label = styled.h2`
+    margin-left: 0;
+    width: 50%;
+    height: 15px;
+    background: ${props => props.theme.colors.shimmerEffect };
+    font-size: 1.1rem;
+    margin-top: 5px;
+    font-weight: 300;
+    border-radius: 3px;
+`
+
+export const RadioCardDiv = styled.div`
+    ${Cover}
+    ${Title}
+    ${Label}
+`
