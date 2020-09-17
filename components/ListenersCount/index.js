ed4bcea67e37ad1e6404532471d1df47d54e1cc4
mateuspntx <mateuspntx@gmail.com>
2020-09-17 19:33:29 -0300

    refactor: remove formatNumber function

--- components/ListenersCount/index.js
+++ components/ListenersCount/index.js
@@ -2,23 +2,13 @@ import { useEffect, useState } from 'react';
 
 import { getListenersCount } from '../../services/api';
 
-const formatNumber = (number) => {
-  return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-};
-
 const ListenersCount = ({ videoId }) => {
   const [listenersCount, setListenersCount] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
   async function getListenersCountData(videoId) {
     const data = await getListenersCount(videoId);
-
-    if (data == undefined) {
-      setListenersCount('No');
-    } else {
-      setListenersCount(data);
-    }
-
+    setListenersCount(data);
     setIsLoading(false);
   }
 
@@ -26,7 +16,7 @@ const ListenersCount = ({ videoId }) => {
     getListenersCountData(videoId);
   }, []);
 
-  return isLoading ? <p>Loading...</p> : <>{formatNumber(listenersCount)}</>;
+  return isLoading ? <p>Loading</p> : <>{listenersCount}</>;
 };
 
 export default ListenersCount;
