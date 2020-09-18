9df7153d8432d403ad1c418fed28df88ff5ad32b
mateuspntx <mateuspntx@gmail.com>
2020-09-17 21:09:16 -0300

    feat: showing Miniplayer when clicks RadioCard

--- components/RadioCard/index.js
+++ components/RadioCard/index.js
@@ -1,10 +1,31 @@
+import { useContext } from 'react';
+
 import { playIcon } from '../../utils/Icons';
 import ListenersCount from '../ListenersCount';
+import { MiniplayerContext } from '../Miniplayer/MiniplayerContext';
 import { Cover, Label, RadioCardDiv, Title } from './styles';
 
 const RadioCard = ({ coverUrl, channelTitle, videoId, showListenersCount }) => {
+  const miniplayerData = useContext(MiniplayerContext);
+
+  const {
+    isShowing,
+    isPlaying,
+    radioName,
+    radioId,
+    radioCoverUrl
+  } = miniplayerData;
+
+  const handleMiniplayer = () => {
+    radioId.set(videoId);
+    radioName.set(channelTitle);
+    radioCoverUrl.set(coverUrl);
+    isShowing.set(true);
+    isPlaying.set(true);
+  };
+
   return (
-    <RadioCardDiv>
+    <RadioCardDiv onClick={() => handleMiniplayer()}>
       <Cover cover={coverUrl}>
         <img src={playIcon} alt="Play" />
       </Cover>
