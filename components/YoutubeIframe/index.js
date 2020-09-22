37f45859d459b96d3fe9dfdffa5adc2e38f71d00
mateuspntx <mateuspntx@gmail.com>
2020-09-22 00:33:30 -0300

    chore: created YoutubeIframe component

---
+++ components/YoutubeIframe/index.js
@@ -0,0 +1,17 @@
+const YoutubeIframe = () => {
+  return (
+    <>
+      <iframe
+        id="youtube__iframe"
+        title="youtube__iframe"
+        type="text/html"
+        width="0"
+        height="0"
+        frameBorder="0"
+        style={{ visibility: 'none' }}
+      ></iframe>
+    </>
+  );
+};
+
+export default YoutubeIframe;
