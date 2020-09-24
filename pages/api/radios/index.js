85eb6a626617b2bd73db62a361ffab46ed7aa78a
mateuspntx <mateuspntx@gmail.com>
2020-09-24 16:56:13 -0300

    chore: fix try/catch block

--- pages/api/radios/index.js
+++ pages/api/radios/index.js
@@ -5,17 +5,17 @@ export default async (req, res) => {
   const key = process.env.YOUTUBE_API_KEY;
 
   const {
-    query: { order, maxResults },
+    query: { order, maxResults }
   } = req;
 
   const defaultParams = `part=snippet&videoCategoryId=10&eventType=live`;
   const url = `https://www.googleapis.com/youtube/v3/search?${defaultParams}&maxResults=${maxResults}&order=${order}&type=video&key=${key}`;
 
-  const data = await fetch(url);
-  const { items } = await data.json();
-
   try {
-    return res.json({ items });
+    const data = await fetch(url);
+    const items = await data.json();
+
+    return res.json(items);
   } catch {
     return res
       .status(400)
