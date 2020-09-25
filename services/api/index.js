9ca45d04f8a14fb8221084646667c2e3d15bcf8d
mateuspntx <mateuspntx@gmail.com>
2020-09-25 20:11:32 -0300

    chore: add vercel env

--- services/api/index.js
+++ services/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:3000'
+  baseURL: process.env.VERCEL_URL || process.env.API_BASE_URL
 });
 
 const LOCAL_STORAGE_NAME = 'LSTNR_cachedRadiosList';
