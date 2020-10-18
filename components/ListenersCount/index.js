58cf0a052e8c62f2edec56f211b26ec991edd360
mateuspntx <mateuspntx@gmail.com>
2020-10-18 13:23:29 -0300

    chore: fix bug avoiding unnecessary render

--- components/ListenersCount/index.js
+++ components/ListenersCount/index.js
@@ -2,21 +2,25 @@ import { useEffect, useState } from 'react';
 
 import { getListenersCount } from '../../services/api';
 
-const ListenersCount = ({ videoId }) => {
-  const [listenersCount, setListenersCount] = useState('');
+const ListenersCount = ({ videoId, showMessage }) => {
+  const [listenersCount, setListenersCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  async function getListenersCountData(videoId) {
-    const data = await getListenersCount(videoId);
-    setListenersCount(data);
-    setIsLoading(false);
-  }
-
   useEffect(() => {
-    getListenersCountData(videoId);
-  }, []);
+    (async () => {
+      const data = await getListenersCount(videoId);
+      setListenersCount(data);
+      setIsLoading(false);
+    })();
+  }, [videoId]);
 
-  return isLoading ? <p>Loading</p> : <>{listenersCount}</>;
+  return isLoading ? (
+    'Loading...'
+  ) : (
+    <>
+      {listenersCount} {showMessage ? 'people listening now' : ''}
+    </>
+  );
 };
 
 export default ListenersCount;
