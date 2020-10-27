3a6ab0ff10998b7a1f93d6d44dbf7bea5263feae
mateuspntx <mateuspntx@gmail.com>
2020-10-26 23:18:13 -0300

    chore: create setFilter function

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
-import { useContext, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import { useContext, useEffect } from 'react';
 
 import AppContainer from '../components/AppContainer';
 import Header from '../components/Header';
@@ -8,32 +7,7 @@ import { MiniplayerContext } from '../components/Miniplayer/MiniplayerContext';
 import RadioCard from '../components/RadioCard';
 import RadioCardSkeleton from '../components/Skeletons/RadioCardSkeleton';
 import { getRadios } from '../services/api';
-
-const Filters = styled.div`
-  margin-top: 6rem;
-  /*width: 1582px;*/
-
-  h1 {
-    font-size: 2.6rem;
-    color: ${(props) => props.theme.colors.primary};
-    transition: color 0.5s, color 0.5s;
-  }
-
-  h3 {
-    font-size: 1.5rem;
-    font-weight: 400;
-    color: ${(props) => props.theme.colors.secondary};
-    transition: color 0.5s, color 0.5s;
-  }
-`;
-
-const RowContainer = styled.div`
-  margin: 1.5rem 0px;
-  display: flex;
-  flex-wrap: wrap;
-  grid-gap: 3.5rem 7%;
-  margin-bottom: 10rem;
-`;
+import { Filters, RowContainer } from '../styles/pages/index';
 
 const Home = () => {
   const miniplayerData = useContext(MiniplayerContext);
@@ -41,6 +15,7 @@ const Home = () => {
   const { radiosList, isLoading } = miniplayerData;
 
   async function getData(params) {
+    isLoading.set(true);
     radiosList.set(await getRadios(params));
     isLoading.set(false);
   }
@@ -54,6 +29,20 @@ const Home = () => {
     });
   }, []);
 
+  const setFilter = (filter) => {
+    if (filter == 'relevance') {
+      getData({
+        needCache: true
+      });
+    } else {
+      getData({
+        query: 'lofi',
+        maxResults: 50,
+        filter
+      });
+    }
+  };
+
   return (
     <AppContainer>
       <Head>
@@ -61,8 +50,12 @@ const Home = () => {
       </Head>
       <Header>
         <Filters>
-          <h1>Trending</h1>
-          <h3>Explore</h3>
+          <button onClick={() => setFilter('relevance')}>
+            <h1 className="h1">Trending</h1>
+          </button>
+          <button onClick={() => setFilter('rating')}>
+            <h3 className="h3">Explore</h3>
+          </button>
         </Filters>
       </Header>
       <RowContainer>
