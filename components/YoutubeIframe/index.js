c5c15bb78032f80817d64bb7e75f35c90a119728
mateuspntx <mateuspntx@gmail.com>
2020-11-02 00:26:54 -0300

    chore: create Iframe props

--- components/YoutubeIframe/index.js
+++ components/YoutubeIframe/index.js
@@ -1,14 +1,18 @@
-const YoutubeIframe = () => {
+const YoutubeIframe = ({ width, height }) => {
+  const style = {
+    visibility: width > 1 ? 'none' : 'visible',
+    paddingRight: '1rem'
+  };
   return (
     <>
       <iframe
         id="youtube__iframe"
         title="youtube__iframe"
         type="text/html"
-        width="0"
-        height="0"
+        width={width}
+        height={height}
         frameBorder="0"
-        style={{ visibility: 'none' }}
+        style={style}
       ></iframe>
     </>
   );
