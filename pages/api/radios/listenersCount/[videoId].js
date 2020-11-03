a01549f366732d761a4504f9e13efa1d0e564f25
mateuspntx <mateuspntx@gmail.com>
2020-11-02 21:23:48 -0300

    chore: fix try/catch block

--- pages/api/radios/listenersCount/[videoId].js
+++ pages/api/radios/listenersCount/[videoId].js
@@ -2,15 +2,15 @@ export default async (req, res) => {
   const key = process.env.YOUTUBE_API_KEY;
 
   const {
-    query: { videoId },
+    query: { videoId }
   } = req;
 
   const url = `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${videoId}&fields=items%2FliveStreamingDetails%2FconcurrentViewers&key=${key}`;
 
-  const data = await fetch(url);
-  const { items } = await data.json();
-
   try {
+    const data = await fetch(url);
+    const { items } = await data.json();
+
     return res.json({ items });
   } catch {
     return res
